c4239cf94f34d45297bdbffe1ea58475722f70bf
HideakiTouhara <hideee6723@gmail.com>
2017-07-07 21:55:41 +0900

    Add search function

--- SearchRepositoryForWantedly/ViewController.swift
+++ SearchRepositoryForWantedly/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
     
     @IBOutlet weak var searchText: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
@@ -16,11 +16,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        searchText.delegate = self
+        searchText.placeholder = "キーワードに関連したRepository名を表示します"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+        
+        if let searchWord = searchBar.text {
+            searchRepo(keyword: searchWord)
+        }
+    }
+    
+    func searchRepo(keyword: String) {
+        let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
+        
+        let URL = Foundation.URL(string: "https://api.github.com/search/repositories?q=\(keyword_encode!)")
+        
+        print(URL)
     }
 
 
